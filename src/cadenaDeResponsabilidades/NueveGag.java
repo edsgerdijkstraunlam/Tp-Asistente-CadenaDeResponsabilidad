e3c28daedb9e8a0e73c8b1ccf7947127e14403b4
Niko Fernandez <ni.ko94@hotmail.com>
2018-06-21 11:56:32 -0300

    Se agrega 9gag

--- src/cadenaDeResponsabilidades/NueveGag.java
+++ src/cadenaDeResponsabilidades/NueveGag.java
@@ -10,31 +10,31 @@ import java.util.ArrayList;
 import asistente.ReceptorDeMensaje;
 
 public class NueveGag implements ReceptorDeMensaje{
-	
+
 	int last;
 	int ini;
 	int fin;
 	ArrayList<String> lista = new ArrayList<String>();
-	String []categorias = {"starwars","pokemon","cute","gaming","relationship","politics","wtf","superhero","science","comic"};	
+	String []categorias = {"starwars","pokemon","cute","gaming","relationship","politics","wtf","superhero","science","comic"};
 	String subStringDir;
 	private ReceptorDeMensaje next ;
-	
+
 	public String getImagen() {
-		
+
 		try {
-			
+
 			System.setProperty("java.net.useSystemProxies", "true"); // sin esto no anda en la facu
 			int categoriaRandom = (int)(Math.random()*categorias.length) ;
 			URL url = new URL("https://9gag.com/"+categorias[categoriaRandom]);
 			InputStream is = url.openStream();
-			
+
 			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-				
+
 				String line;
 				while ((line = br.readLine()) != null) {
-					
+
 					last = line.indexOf("\"data\":");
-					
+
 					if (last != -1) {
 
 						while (line.contains("\"type\":\"Photo\"") ){
@@ -43,9 +43,9 @@ public class NueveGag implements ReceptorDeMensaje{
 							fin = line.indexOf(".jpg") + 4;
 							int principio = line.indexOf("\"url\":\"");
 							subStringDir = line.substring(principio+7, fin);
-							
-							//subStringDir = subStringDir.replace("\\", "") ;
-							System.out.println(subStringDir);
+
+							subStringDir = subStringDir.replace("\\", "") ;
+							//System.out.println(subStringDir);
 							lista.add(subStringDir);
 							line = line.substring(fin);
 						}
@@ -56,12 +56,12 @@ public class NueveGag implements ReceptorDeMensaje{
 
 			}
 		} catch (IOException e) {
-			
+
 			return null;
 		}
 
 		if (!lista.isEmpty()) {
-			
+
 			String direccion = lista.get((int)(Math.random()*lista.size()));
 			lista.clear();
 			return direccion;
@@ -69,11 +69,11 @@ public class NueveGag implements ReceptorDeMensaje{
 
 		return null;
 	}
-	
-	
+
+
 	@Override
 	public String escuchar(String msg, String usuario) {
-		
+
 		String resp;
 
 		if (msg.contains("9gag")) {
